601598974f5411af4ddfab014bd440b3f6bd844b
AStudentOfDread <toastybreadbread@gmail.com>
2022-02-27 00:02:39 -0600

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using HarmonyLib;
+using ModLoader;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace ASoD_s_VanillaUpgrades
+{
+    public class Main
+    {
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public string getModAuthor()
+		{
+			return "ASoD";
+		}
+
+		// Token: 0x06000002 RID: 2 RVA: 0x00002057 File Offset: 0x00000257
+		public string getModName()
+		{
+			return "VanillaUpgrades";
+		}
+
+		// Token: 0x06000003 RID: 3 RVA: 0x0000205E File Offset: 0x0000025E
+		public void load()
+		{
+			Main.patcher = new Harmony("mods.ASoD.VanUp");
+			Main.patcher.PatchAll();
+			Loader.modLoader.suscribeOnChangeScene(new UnityAction<Scene, LoadSceneMode>(this.OnSceneLoaded));
+		}
+
+		// Token: 0x06000004 RID: 4 RVA: 0x00002090 File Offset: 0x00000290
+		private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+		{
+			if (scene.name == "Build_PC")
+			{
+				GameObject gameObject = new GameObject("ASoDBuildObject");
+				gameObject.AddComponent<Menu>();
+				UnityEngine.Object.DontDestroyOnLoad(gameObject);
+				gameObject.SetActive(true);
+				Main.menuObject = gameObject;
+				return;
+			}
+			UnityEngine.Object.Destroy(Main.menuObject);
+		}
+
+		public void unload()
+		{
+			throw new NotImplementedException();
+		}
+
+		// Token: 0x04000001 RID: 1
+		public static GameObject menuObject;
+
+		// Token: 0x04000002 RID: 2
+		public static Harmony patcher;
+	}
+}
---
+++ Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HarmonyLib;
+using ModLoader;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using SFS.Builds;
+
+namespace ASoD_s_VanillaUpgrades
+{
+    internal class Menu : MonoBehaviour
+    {
+        // Token: 0x0400136C RID: 4972
+        public static Rect windowRect = new Rect((float)Screen.width * 0.05f, (float)Screen.height * 0.92f, 200f, 200f);
+
+        // Token: 0x0400136D RID: 4973
+        public static bool snapping;
+
+        // Token: 0x0400136E RID: 4974
+        public static bool noAdaptation;
+
+        public static void windowFunc(int windowID)
+        {
+            snapping = GUI.Toggle(new Rect(30f, 20f, 160f, 20f), snapping, " Disable Snapping");
+            noAdaptation = GUI.Toggle(new Rect(30f, 40f, 160f, 20f), noAdaptation, " Disable Adapting");
+        }
+
+        public static void OnGUI()
+        {
+            windowRect = GUI.Window(0, windowRect, new GUI.WindowFunction(windowFunc), "Build Settings");
+            Event current = Event.current;
+            bool flag = current.keyCode == KeyCode.L;
+            if (flag)
+            {
+                BuildManager.main.Launch();
+            }
+        }
+    }
+}
