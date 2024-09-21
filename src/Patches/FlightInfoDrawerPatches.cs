a48b0e56ace7b311f8a985934a9339892a59f8a7
Neptune-Sky <68938422+Neptune-Sky@users.noreply.github.com>
2024-09-20 21:18:56 -0500

    Update FlightInfoDrawerPatches.cs

--- src/Patches/FlightInfoDrawerPatches.cs
+++ src/Patches/FlightInfoDrawerPatches.cs
@@ -22,51 +22,45 @@ namespace VanillaUpgrades
         // - TWR is now the local Thrust-To-Weight ratio (takes into account the local gravity)
         private static void Postfix(ref TextAdapter ___thrustText, ref TextAdapter ___thrustToWeightText)
         {
-            if (PlayerController.main.player.Value is Rocket rocket)
+            if (PlayerController.main.player.Value is not Rocket rocket) return;
+            var mass = rocket.rb2d.mass;
+            var localGravity = (float)(rocket.location.planet.Value.GetGravity(rocket.location.position.Value.magnitude));
+
+            // Calculate thrust, taking into account engines state, stretching and orientation
+            var thrust = new Vector2(0.0f, 0.0f);
+
+            foreach (var engineModule in rocket.partHolder.GetModules<EngineModule>())
             {
-                float mass = rocket.rb2d.mass;
-                float localGravity = (float)(rocket.location.planet.Value.GetGravity(rocket.location.position.Value.magnitude));
+                if (!engineModule.engineOn.Value) continue;
+                var direction = (Vector2)engineModule.transform.TransformVector(engineModule.thrustNormal.Value);
 
-                // Calculate thrust, taking into account engines state, stretching and orientation
-                Vector2 thrust = new Vector2(0.0f, 0.0f);
+                // For cheaters...
+                var stretchFactor = 1.0f;
+                if (Base.worldBase.AllowsCheats) stretchFactor = direction.magnitude;
 
-                foreach (EngineModule engineModule in rocket.partHolder.GetModules<EngineModule>())
-                {
-                    if (engineModule.engineOn.Value)
-                    {
-                        Vector2 direction = (Vector2)engineModule.transform.TransformVector(engineModule.thrustNormal.Value);
-
-                        // For cheaters...
-                        float stretchFactor = 1.0f;
-                        if (Base.worldBase.AllowsCheats) stretchFactor = direction.magnitude;
-
-                        Vector2 engineThrust = engineModule.thrust.Value * direction.normalized * stretchFactor * engineModule.throttle_Out.Value;
-                        thrust += engineThrust;
-                    }
-                }
-
-                foreach (BoosterModule boosterModule in rocket.partHolder.GetModules<BoosterModule>())
-                {
-                    if(boosterModule.enabled)
-                    {
-                        Vector2 direction = (Vector2)boosterModule.transform.TransformVector(boosterModule.thrustVector.Value); // equal to thrust * stretch factor in magnitude
-
-                        // For cheaters...
-                        float stretchFactor = 1.0f;
-                        if (Base.worldBase.AllowsCheats) stretchFactor = direction.magnitude / (float)boosterModule.thrustVector.Value.magnitude;
-
-                        // adjusting direction to what it should be now
-                        direction = direction.normalized;
-
-                        thrust += boosterModule.thrustVector.Value.magnitude * direction.normalized * stretchFactor;
-                    }
-                }
-
-                float thrustVal = thrust.magnitude;
-
-                ___thrustText.Text = thrustVal.ToThrustString().Split(':')[1];
-                ___thrustToWeightText.Text = (thrustVal / mass * 9.8f / localGravity).ToTwrString().Split(':')[1];
+                var engineThrust = engineModule.thrust.Value * direction.normalized * stretchFactor * engineModule.throttle_Out.Value;
+                thrust += engineThrust;
             }
+
+            foreach (var boosterModule in rocket.partHolder.GetModules<BoosterModule>())
+            {
+                if (!boosterModule.enabled) continue;
+                var direction = (Vector2)boosterModule.transform.TransformVector(boosterModule.thrustVector.Value); // equal to thrust * stretch factor in magnitude
+
+                // For cheaters...
+                var stretchFactor = 1.0f;
+                if (Base.worldBase.AllowsCheats) stretchFactor = direction.magnitude / (float)boosterModule.thrustVector.Value.magnitude;
+
+                // adjusting direction to what it should be now
+                direction = direction.normalized;
+
+                thrust += boosterModule.thrustVector.Value.magnitude * direction.normalized * stretchFactor;
+            }
+
+            var thrustVal = thrust.magnitude;
+
+            ___thrustText.Text = thrustVal.ToThrustString().Split(':')[1];
+            ___thrustToWeightText.Text = (thrustVal / mass * 9.8f / localGravity).ToTwrString().Split(':')[1];
         }
     }
 }
