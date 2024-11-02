eac26d81cf75326fab725d76c9986821931c99c3
Neptune-Sky <68938422+Neptune-Sky@users.noreply.github.com>
2024-11-02 00:54:52 -0500

    that shouldve been there to begin with

--- src/Patches/WorldPatches.cs
+++ src/Patches/WorldPatches.cs
@@ -143,4 +143,23 @@ namespace VanillaUpgrades
             ___timewarpText.Text = WorldTime.main.timewarpSpeed.Round(2) + "x";
         }
     }
+    
+    [HarmonyPatch(typeof(Rocket), "GetTorque")]
+    public static class ToggleTorque
+    {
+        public static bool disableTorque;
+
+        public static void Toggle()
+        {
+            if (!PlayerController.main.HasControl(MsgDrawer.main)) return;
+            disableTorque = !disableTorque;
+            MsgDrawer.main.Log("Torque " + (disableTorque ? "Disabled" : "Enabled"));
+        }
+        private static bool Prefix(ref float __result)
+        {
+            if (!disableTorque) return true;
+            __result = 0f;
+            return false;
+        }
+    }
 }
